be8567e1aaf51fdf715f4da76545d849da9fa0c6
montumakwana999 <montumakwana999@gmail.com>
2026-03-07 20:02:38 +0530

    Add files via upload

---
+++ 27-for.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    for(i=1;i<=2;i++)
+    {
+        for(l=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
